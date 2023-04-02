36039c3a6e22de20a0cfbd51a9bec1dfdf13ce9e
Guido W. Pettinari <coccoinomane@gmail.com>
2023-04-02 11:30:14 +0200

    ♻️ Helper file

--- scripts/eth-liq.py
+++ scripts/eth-liq.py
@@ -1,55 +1,46 @@
 from time import time
+from typing import Any, Dict
 
 from brownie import Contract, Token, accounts, network
-from brownie.utils import color
+from scripts.helper import (
+    approve_token_if_needed,
+    build_tx_params,
+    check_live,
+    deploy_or_fetch_token,
+    fetch_contract,
+    info,
+    load_account,
+)
 
 
 def main(
     router_address: str,
-    account: str,
+    account_file: str,
     tx_type: int = 0,  # TODO: not working?
     token_address: str = None,
     ask_before_liq: bool = True,
     value_eth: int = 10**13,
     value_token: int = 10**18,
 ) -> None:
-    """
-    Deploy a test token and and add liquidity for it on a
+    """Deploy a test token and and add liquidity for it on a
     Uniswap-like router.
 
     Example:
         # Add liquidity on Arbitrum Sushiswap with wallet 'my-account'
         brownie run eth-liq main 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506 my-account --network arbitrum-main
     """
-    # Check whether the user is going live
-    if not network.show_active() == "development":
-        warn(
-            f"WARNING: You are on live chain {network.show_active()}. Continue? [y/N]:"
-        )
-        if input().lower() != "y":
-            return
+    check_live()
+    router = fetch_contract(router_address, "router")
+    account = load_account(account_file)
+    tx_params = build_tx_params(account, tx_type)
+    token = deploy_or_fetch_token(token_address=token_address, tx_params=tx_params)
+    approve_token_if_needed(token, router, value_token, tx_params)
 
-    info(f"FETCHING ROUTER {router_address} FROM EXPLORER")
-    router = Contract.from_explorer(router_address)
-    info(f"ROUTER {router._name} LOADED")
-    info(f"LOADING ACCOUNT {account} ON NETWORK {network.show_active()}")
-    accounts.load(account)
-    tx_params = {"from": accounts[0]}
-    if tx_type:
-        tx_params["type"] = tx_type
-    if not token_address:
-        info(f"DEPLOYING TEST TOKEN")
-        token = Token.deploy("Test token", "TST", 18, 1e9 * 1e18, tx_params)
-    else:
-        info(f"FETCHING TOKEN {token_address} FROM EXPLORER")
-        token = Contract.from_explorer(token_address)
-    info(f"TOKEN ADDRESS: {token.address}")
-    info(f"APPROVING ROUTER TO SPEND TOKENS")
-    token.approve(router_address, value_token, tx_params)
     if ask_before_liq:
         info(f"ADD LIQUIDITY? [Y/n]:")
         if input().lower() == "n":
             return
+
     info(f"ADDING LIQUIDITY")
     router.addLiquidityETH(
         token,
@@ -60,11 +51,3 @@ def main(
         time() + 60,
         tx_params | {"value": value_eth},
     )
-
-
-def info(text: str, col: str = "cyan") -> None:
-    print(f"{color(col)}{text}{color}")
-
-
-def warn(text: str, col: str = "magenta") -> None:
-    print(f"{color(col)}{text}{color}")
--- scripts/token.py
+++ scripts/token.py
@@ -1,5 +1,18 @@
-from brownie import Token, accounts
+from scripts.helper import (
+    build_tx_params,
+    check_live,
+    deploy_or_fetch_token,
+    load_account,
+)
 
 
-def main(name: str = "Test Token", symbol: str = "TST") -> None:
-    return Token.deploy(name, symbol, 18, 1e9 * 1e18, {"from": accounts[0]})
+def main(
+    account_file: str,
+    tx_type: int = 0,  # TODO: not working?
+    token_address: str = None,
+) -> None:
+    """Deploy a test token"""
+    check_live()
+    account = load_account(account_file)
+    tx_params = build_tx_params(account, tx_type)
+    deploy_or_fetch_token(token_address=token_address, tx_params=tx_params)
